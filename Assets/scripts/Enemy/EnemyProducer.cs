9d88ec4c4721336589c49d02f6d22902f1dcca3c
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2021-04-25 16:24:52 +0300

    Continue

--- Assets/scripts/Enemy/EnemyProducer.cs
+++ Assets/scripts/Enemy/EnemyProducer.cs
@@ -2,41 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = System.Object;
 
 public class EnemyProducer : MonoBehaviour
 {
-    [SerializeField] private GameObject enemy1;
+    [SerializeField] private GameObject[] enemies;
 
-    [SerializeField] private GameObject enemy2;
+    [SerializeField] private DateTime lastUpdate;
 
-    [SerializeField] private GameObject enemy3;
-
-    // Start is called before the first frame update
     void Start()
     {
-		var enemyNum = UnityEngine.Random.Range(1, 4);
-		switch(enemyNum) {
-			case 1: 
-				Instantiate(enemy1, 
-					new Vector3(UnityEngine.Random.Range(-10, 10), 0, UnityEngine.Random.Range(-10, 10)), 
-					Quaternion.identity);
-				break;
-			case 2: 
-				Instantiate(enemy2, 
-					new Vector3(UnityEngine.Random.Range(-10, 10), 0, UnityEngine.Random.Range(-10, 10)), 
-					Quaternion.identity);
-			break;
-			default:
-				Instantiate(enemy3, 
-					new Vector3(UnityEngine.Random.Range(-10, 10), 0, UnityEngine.Random.Range(-10, 10)), 
-					Quaternion.identity);
-			break;
-		}      
+	    lastUpdate = DateTime.Now;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+	    if (DateTime.Now.Second - lastUpdate.Second >= 1)
+	    {
+		    var i = UnityEngine.Random.Range(0, this.enemies.Length);
+		    Instantiate(enemies[i],
+			    new Vector3(UnityEngine.Random.Range(-10, 10), 0, UnityEngine.Random.Range(-10, 10)),
+			    Quaternion.identity);
+		    lastUpdate = DateTime.Now;
+	    }
     }
 }
